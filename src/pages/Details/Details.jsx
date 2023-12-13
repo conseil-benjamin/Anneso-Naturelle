8b69a4f077242279190c304b536c1a1c7060e937
conseil-benjamin <benlenormand61@gmail.com>
2023-12-13 13:28:29 +0100

    finish to fetch favoris & navigate to a product who his in the favorite list

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,27 +1,30 @@
-import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import "./Details.css";
 import CareScale from "../../components/CareScale/CareScale";
-import { useState, useEffect } from "react";
 import { Loader } from "../../utils/Loader";
 
 function Details({ cart, updateCart }) {
   const navigate = useNavigate();
+  const { id } = useParams();
+
   const [isDataLoading, setDataLoading] = useState(false);
   const [productAdd, setProductAdd] = useState(false);
   const [isAddElement, setAddElement] = useState(false);
   const [produit, setProduit] = useState([]);
-  const { id } = useParams();
+  const tableauObjet = Object.values(produit);
 
   useEffect(() => {
     const fetchData = async () => {
       setDataLoading(true);
       try {
         const response = await fetch(
-          "http://localhost:5000/api/v1/products/" + id
+          `http://localhost:5000/api/v1/products/${id}`
         );
         const product = await response.json();
         setProduit(product);
         console.log(product);
+        console.log(produit);
       } catch (error) {
         console.error(error);
       } finally {
@@ -29,40 +32,44 @@ function Details({ cart, updateCart }) {
       }
     };
     fetchData();
-  }, []);
+  }, [id]);
 
   function addToCart(name, price) {
     const currentPlantSaved = cart.find((plant) => plant.name === name);
-    if (currentPlantSaved) {
-      const cartFilteredCurrentPlant = cart.filter(
-        (plant) => plant.name !== name
-      );
-      updateCart([
-        ...cartFilteredCurrentPlant,
-        { name, price, amount: currentPlantSaved.amount + 1 },
-      ]);
-      setProductAdd(true);
-    } else {
-      updateCart([...cart, { name, price, amount: 1 }]);
-      setProductAdd(true);
-    }
+    const updatedCart = currentPlantSaved
+      ? cart.filter((plant) => plant.name !== name)
+      : [...cart, { name, price, amount: 0 }];
+
+    updateCart([
+      ...updatedCart,
+      {
+        name,
+        price,
+        amount: currentPlantSaved ? currentPlantSaved.amount + 1 : 1,
+      },
+    ]);
+
+    setProductAdd(true);
     setAddElement(true);
-    productAdd && navigate("/Panier");
-    setProductAdd(false);
+
+    if (productAdd) {
+      navigate("/Panier");
+      setProductAdd(false);
+    }
   }
 
   return (
     <div className="bodyElementDetails">
       {isDataLoading ? (
-        <Loader />
+        <div className="loader-div">
+          <Loader />
+        </div>
       ) : (
         <>
           <div className="return-to-collection">
             <Link to="/collections">
               <img
-                src={
-                  "https://res.cloudinary.com/dc1p20eb2/image/upload/v1700322943/Icon_retour_arriere.png"
-                }
+                src="https://res.cloudinary.com/dc1p20eb2/image/upload/v1700322943/Icon_retour_arriere.png"
                 alt="Description de l'image"
                 width={25}
                 height={25}
@@ -70,35 +77,42 @@ function Details({ cart, updateCart }) {
             </Link>
           </div>
           <div className="container-left-details">
-            <img
-              src={produit.cover}
-              width={250}
-              height={250}
-              alt="Image plante"
-              id="image_produit"
-            ></img>
+            {tableauObjet.map((produit) => (
+              <img
+                key={produit.id}
+                src={produit.cover}
+                width={250}
+                height={250}
+                alt="Image plante"
+                id="image_produit"
+              />
+            ))}
           </div>
           <div className="details-list">
-            <h1>{produit.name}</h1>
-            <p>Nom : {produit.name}</p>
-            <p>Plante {produit.category}</p>
-            <div className="careScaleDetails">
-              <p> Echelle d'arrosage nécessaire : (1-3) </p>
-              <CareScale careType="water" scaleValue={produit.water} />
-            </div>
-            <div className="careScaleDetails">
-              <p> Echelle de luminosité nécessaire : (1-3) </p>
-              <CareScale careType="light" scaleValue={produit.light} />
-            </div>
-            <p>Prix : {produit.price} €</p>
-            <p className="description">{produit.description}</p>
-            <button
-              class="button-56"
-              role="button"
-              onClick={() => addToCart(produit.name, produit.price)}
-            >
-              Ajouter au panier
-            </button>
+            {tableauObjet.map((produit) => (
+              <div key={produit.id}>
+                <h1>{produit.name}</h1>
+                <p>Nom : {produit.name}</p>
+                <p>Plante {produit.category}</p>
+                <div className="careScaleDetails">
+                  <p> Echelle d'arrosage nécessaire : (1-3) </p>
+                  <CareScale careType="water" scaleValue={produit.water} />
+                </div>
+                <div className="careScaleDetails">
+                  <p> Echelle de luminosité nécessaire : (1-3) </p>
+                  <CareScale careType="light" scaleValue={produit.light} />
+                </div>
+                <p>Prix : {produit.price} €</p>
+                <p className="description">{produit.description}</p>
+                <button
+                  className="button-56"
+                  role="button"
+                  onClick={() => addToCart(produit.name, produit.price)}
+                >
+                  Ajouter au panier
+                </button>
+              </div>
+            ))}
           </div>
         </>
       )}
