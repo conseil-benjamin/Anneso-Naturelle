7048db2551393065b4eba6bcea8ed546ac35e931
Benjamin <benlenormand61@gmail.com>
2024-02-17 19:30:27 +0100

    Implemented password reset functionality in ResetPassword.jsx. Pending task: Complete page design for ResetPassword.jsx.

--- src/pages/ResetPassword/ResetPassword.jsx
+++ src/pages/ResetPassword/ResetPassword.jsx
@@ -3,9 +3,10 @@ import {useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
 import Swal from "sweetalert2";
+import validator from "validator";
 
-function ResetPassword(){
-    const { token } = useParams();
+function ResetPassword() {
+    const {token} = useParams();
     const [tokenIsValid, setTokenIsValid] = useState(false);
     const [nouveauMotDePasse, setNouveauMotDePasse] = useState("");
     const [confirmationNouveauMotDePasse, setConfirmationNouveauMotDePasse] = useState("");
@@ -28,10 +29,10 @@ function ResetPassword(){
                         },
                     },
                 );
-                if (response.ok){
+                if (response.ok) {
                     const token = await response.json();
                     setTokenIsValid(true);
-                } else{
+                } else {
                     console.log(response)
                     setTokenIsValid(false);
                     Swal.fire({
@@ -42,17 +43,71 @@ function ResetPassword(){
                     })
                     setTimeout(() => {
                         window.location.href = "/auth/login";
-                    }, 150000000)
+                    }, 4000)
                 }
             } catch (error) {
                 console.error(error)
-            }
-            finally {
+            } finally {
                 setDataLoading(false);
             }
         }
         tokenIsValid().then();
     }, [token])
+
+    const resetPassword = () => {
+        if (nouveauMotDePasse !== confirmationNouveauMotDePasse || validator.isEmpty(nouveauMotDePasse)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Les mots de passe ne correspondent pas.',
+            })
+            return;
+        } else if (!validator.isStrongPassword(nouveauMotDePasse)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Le mot de passe ne respecte pas les conditions de sécurité. Veuillez vérifier que votre mot de passe contient ceci : 1 masjuscule, miniscule, chiffre, caractère spécial et un total de 8 caractère au minimum.',
+            })
+            return;
+        }
+        const resetPassword = async () => {
+            try {
+                setDataLoading(true);
+                const response = await fetch(
+                    `http://localhost:5000/api/v1/auth/reset-password/${token}`, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            nouveauMotDePasse: nouveauMotDePasse,
+                        }),
+                    },
+                );
+                if (response.ok) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Mot de passe réinitialisé',
+                        text: 'Votre mot de passe a été réinitialisé avec succès. Vous pouvez maintenant vous connecter avec votre nouveau mot de passe.',
+                    })
+                    setTimeout(() => {
+                        window.location.href = "/auth/login";
+                    }, 4000)
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Erreur lors de la réinitialisation du mot de passe',
+                    })
+                }
+            } catch (error) {
+                console.error(error)
+            } finally {
+                setDataLoading(false);
+            }
+        }
+        resetPassword().then();
+    }
 
     /**
      * TODO : Faire une recherche sur un user pour vérifier que le token est encore valide
@@ -64,29 +119,42 @@ function ResetPassword(){
     return <>
         {tokenIsValid ?
             <>
-                <div className="div-password">
+                <div style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    margin: "2em"
+                }}>
+                    <h2>Réinitialisation de votre mot de passe</h2>
+                    <div className="div-password">
+                        <input
+                            className="input-login"
+                            type={inputType}
+                            placeholder="Nouveau mot de passe"
+                            value={nouveauMotDePasse}
+                            onChange={(e) => setNouveauMotDePasse(e.target.value)}
+                        ></input>
+                        {inputType === "password" ? (
+                            <FontAwesomeIcon icon={faEye} id={"icon-eye-see-password"}
+                                             onClick={togglePasswordVisibility}></FontAwesomeIcon>
+                        ) : (
+                            <FontAwesomeIcon icon={faEyeSlash} id={"icon-eye-see-password"}
+                                             onClick={togglePasswordVisibility}></FontAwesomeIcon>
+                        )}
+                    </div>
                     <input
                         className="input-login"
                         type={inputType}
-                        placeholder="Nouveau mot de passe"
-                        value={nouveauMotDePasse}
-                        onChange={(e) => setNouveauMotDePasse(e.target.value)}
+                        placeholder="Confirmer le nouveau mot de passe"
+                        value={confirmationNouveauMotDePasse}
+                        onChange={(e) => setConfirmationNouveauMotDePasse(e.target.value)}
                     ></input>
-                    {inputType === "password" ? (
-                        <FontAwesomeIcon icon={faEye} id={"icon-eye-see-password"}
-                                         onClick={togglePasswordVisibility}></FontAwesomeIcon>
-                    ) : (
-                        <FontAwesomeIcon icon={faEyeSlash} id={"icon-eye-see-password"}
-                                         onClick={togglePasswordVisibility}></FontAwesomeIcon>
-                    )}
+                    <button onClick={() => resetPassword()}>
+                        Réinitialiser le mot de passe
+                    </button>
                 </div>
-                <input
-                    className="input-login"
-                    type={inputType}
-                    placeholder="Confirmer le nouveau mot de passe"
-                    value={confirmationNouveauMotDePasse}
-                    onChange={(e) => setConfirmationNouveauMotDePasse(e.target.value)}
-                ></input>
+
             </>
             : null
         }
